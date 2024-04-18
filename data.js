bab4e7fab884e53449d8e8057fdcb94a3425f8f9
Aniket <vishnoianiket4@gmail.com>
2024-04-18 19:24:19 +0530

    basic grid structed created[first commit]

---
+++ data.js
@@ -0,0 +1,285 @@
+
+export const data = [
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+    {
+        date:"22.07.2021",
+        time:"16:02 PM",
+        name: "App_Url_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+        
+    },
+]
